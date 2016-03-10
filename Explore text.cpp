99e039adce7454b227ab6678d2b3d3ae77b2bd65
vladpereskokov <vladpereskokov@mail.ru>
2016-03-10 22:12:31 +0300

    Update Explore text.cpp

--- Explore text.cpp
+++ Explore text.cpp
@@ -3,32 +3,39 @@
 #include <fstream>
 #include <map>
 
+template<typename T>
+T search_word(T &map, const std::string &string)
+{
+    std::string signs = ".,!?;:|{}[]<>~`()*&^%$#@+- ";
+    for (std::string::size_type pos = 0;
+         (pos = string.find_first_not_of(signs, pos)) != std::string::npos;)
+    {
+        auto n = pos;
+        pos = string.find_first_of(signs, pos);
+        std::string word = string.substr(n, pos == std::string::npos ? pos : pos - n);
+        ++map[word];
+    }
+    return map;
+}
+
 void explore(std::fstream &file)
 {
-	std::string string, signs = ".,!?;:|{}[]<>~`()*&^%$#@+- ";
-	std::map<std::string, size_t> Occurence;
-	while (file >> string)
-	{
-		for (std::string::size_type pos = 0;
-		(pos = string.find_first_not_of(signs, pos)) != std::string::npos;)
-		{
-			auto n = pos;
-			pos = string.find_first_of(signs, pos);
-			std::string word = string.substr(n, pos == std::string::npos ? pos : pos - n);
-			++Occurence[word];
-		}
-	}
-	for (const auto &temp : Occurence)
-		std::cout << "Number of < " << temp.first << " > in text is : " << temp.second << std::endl;
+    std::string string;
+    std::map<std::string, size_t> occurence;
+    while (file >> string)
+    {
+        search_word(occurence, string);
+    }
+    for (const auto &temp : occurence)
+        std::cout << "Number of < " << temp.first << " > in text is : " << temp.second << std::endl;
 }
 
 int main()
 {
-	std::string fileName;
-	std::cin >> fileName;
-	std::fstream file(fileName);
-	explore(file);
-	file.close();
-	system("pause");
-	return 0;
+    std::string fileName;
+    std::cin >> fileName;
+    std::fstream file(fileName);
+    explore(file);
+    file.close();
+    return 0;
 }
